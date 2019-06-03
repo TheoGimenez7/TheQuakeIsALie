0b056d5b37033d9b383c541dd277d794e1e6f78c
Loic SCHNURIGER <schnuril@iut-acy.local>
2019-06-03 14:39:23 +0200

    Revert "Moi"
    This reverts commit e447076aab05090ec838a38b67fd6d8520f38c9b.

---
+++ Assets/Scripts/Menu/Couleur_Taille_Son.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+    var couleurEntrer : Color = Color.green;
+    var couleurSortie : Color = Color.white;
+    var tailleEntrer : float = 45;
+    var tailleSortie : float = 45;
+public class Couleur_Taille_Son : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
