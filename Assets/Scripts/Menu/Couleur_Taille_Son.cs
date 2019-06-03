a55921f27ad0d24c0cf2ec9dcc7443e5e9e23035
Loic SCHNURIGER <schnuril@iut-acy.local>
2019-06-03 14:16:31 +0200

    Correction Script

--- Assets/Scripts/Menu/Couleur_Taille_Son.cs
+++ Assets/Scripts/Menu/Couleur_Taille_Son.cs
@@ -2,6 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
+    var couleurEntrer : Color = Color.green;
+    var couleurSortie : Color = Color.white;
+    var tailleEntrer : float = 45;
+    var tailleSortie : float = 45;
 public class Couleur_Taille_Son : MonoBehaviour {
 
 	// Use this for initialization
