d67899edae49d16e3236196315a268126f78cc1e
Antoine01100 <45842834+Antoine01100@users.noreply.github.com>
2019-06-03 14:00:12 +0200

    Update

--- Assets/Scripts/poubelle.cs
+++ Assets/Scripts/poubelle.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Materialtest : MonoBehaviour {
+public class poubelle : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
