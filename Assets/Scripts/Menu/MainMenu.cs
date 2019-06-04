2160d601b117aafc6bb0d884fa990e0fd14b7afa
Loic SCHNURIGER <schnuril@iut-acy.local>
2019-06-04 16:42:42 +0200

    Repush

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -8,7 +8,7 @@ public class MainMenu : MonoBehaviour {
 
     public void PlayGame ()
     {
-        SceneManager.LoadScene("TheQuakeIsALie", LoadSceneMode.Single);
+        SceneManager.LoadScene("AI", LoadSceneMode.Single);
     }
 
     public void ExitGame()
