a7f7d81f728d72f4ef83535e85f831a908e39e0b
DomPouleto <45224847+DomPouleto@users.noreply.github.com>
2019-06-04 11:31:46 +0200

    Perso 3 Network OK

--- Assets/Scripts/SupprimerProjectile.cs
+++ Assets/Scripts/SupprimerProjectile.cs
@@ -8,9 +8,6 @@ public class SupprimerProjectile : MonoBehaviour
     // Use this for initialization
     private void OnCollisionEnter(Collision collision)
     {
-        ContactPoint contact = collision.contacts[0];
-        Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
-        Vector3 pos = contact.point;
         Destroy(gameObject);
     }
 
