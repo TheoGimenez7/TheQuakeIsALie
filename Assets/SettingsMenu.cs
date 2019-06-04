5f2b8e2eccc5417b869be0dc9ef820fd3f7aa424
Loic SCHNURIGER <schnuril@iut-acy.local>
2019-06-04 14:54:08 +0200

    Menu

--- Assets/SettingsMenu.cs
+++ Assets/SettingsMenu.cs
@@ -1,13 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
 
 public class SettingsMenu : MonoBehaviour {
 
+
+    public AudioMixer audioMixer;
+
+
+
     public void SetVolume(float volume)
     {
-
-        Debug.Log(volume);
+        audioMixer.SetFloat("volume", volume);
             
     }
 }
