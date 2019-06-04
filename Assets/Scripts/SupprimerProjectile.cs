3e8b043272f60f7ea99074f818692a8bcf9d2af5
DomPouleto <45224847+DomPouleto@users.noreply.github.com>
2019-06-04 10:55:32 +0200

    Player V1 fixed (en partie)

--- Assets/Scripts/SupprimerProjectile.cs
+++ Assets/Scripts/SupprimerProjectile.cs
@@ -6,11 +6,12 @@ public class SupprimerProjectile : MonoBehaviour
 {
 
     // Use this for initialization
-    private void OnCollisionEnter(Collision col)
+    private void OnCollisionEnter(Collision collision)
     {
+        ContactPoint contact = collision.contacts[0];
+        Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
+        Vector3 pos = contact.point;
         Destroy(gameObject);
-        Debug.Log(col);
-
     }
 
 }
