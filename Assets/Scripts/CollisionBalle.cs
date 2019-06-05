528e6c19ed9e78689e07aac2f32849a7ce512d77
Antoine01100 <45842834+Antoine01100@users.noreply.github.com>
2019-06-05 14:48:55 +0200

    Collision respawn

--- Assets/Scripts/CollisionBalle.cs
+++ Assets/Scripts/CollisionBalle.cs
@@ -10,18 +10,28 @@ public class CollisionBalle : MonoBehaviour {
     private AudioClip[] deathSound;
     private AudioSource audioSource;
 
+    [SerializeField]
+    private Behaviour[] disableOnDeath;
+    private bool[] wasEnabled;
+
     public void Start()
     {
         audioSource = GetComponent<AudioSource>();
     }
 
   
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision collision, string _playerID)
     {
         if (collision.gameObject.tag == "Projectile")
         {
-            Debug.Log("PTDR ! T MOR :')))))) ");
             audioSource.PlayOneShot(deathSound[Random.Range(0, deathSound.Length)]);
+            Player _player = GameManager.GetPlayer(_playerID);
+            Respawn();
         }
+    }
+
+    private void Respawn()
+    {
+        GameObject[] spawnPoints1 = GetComponents(Spawnpoint);
     }
 }
