25e98a25ca799c370faae758e777e3fd20e4eb30
Just1bridou <45208166+Just1bridou@users.noreply.github.com>
2019-06-06 16:50:01 +0200

    parcour :dab:

---
+++ Assets/test_final.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test_final : MonoBehaviour {
+    public GameObject player;
+    public Transform Destination;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag=="player")
+        {
+            player.transform.position = Destination.transform.position;
+        }
+    }
+}
