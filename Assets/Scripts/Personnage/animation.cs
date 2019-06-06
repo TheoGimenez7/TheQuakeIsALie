ced3d6e2a3dff48ea132c14ba49e35b0bfb3e909
Vincent LEMETTAIS <lemettav@iut-acy.local>
2019-06-06 10:53:00 +0200

    anim

---
+++ Assets/Scripts/Personnage/animation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animation : MonoBehaviour {
+
+    private Animator Anim;
+	// Use this for initialization
+	void Start () {
+        Anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Z))
+        {
+            Anim.SetBool("run", true);
+        }
+	}
+}
