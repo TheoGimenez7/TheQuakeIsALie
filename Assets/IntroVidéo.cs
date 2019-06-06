31b8cdf103e06d14da7dec35cbf79f96e3cd7008
Loic SCHNURIGER <schnuril@iut-acy.local>
2019-06-06 08:02:49 +0200

    Vidéo

---
+++ Assets/IntroVidéo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class IntroVidéo : MonoBehaviour {
+
+    public void SkipVideo()
+    {
+        SceneManager.LoadScene("MenuV3", LoadSceneMode.Single);
+    }
+}
