88190ffa8a62210cbf8cc4cbdcc8d173b8ffc312
Antoine01100 <45842834+Antoine01100@users.noreply.github.com>
2019-06-06 08:53:35 +0200

    Plus Viser

--- Assets/Scripts/Viser.cs
+++ Assets/Scripts/Viser.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -24,4 +24,4 @@ using UnityEngine;
                 transform.localPosition = NormalPos;
             }
         }
-    }
+    }*/
