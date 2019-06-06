ce4a28fdd5b7ff1c0f6f41d5dc9a94808d74a474
Just1bridou <45208166+Just1bridou@users.noreply.github.com>
2019-06-06 22:02:27 +0200

    update mapv1

---
+++ Assets/informations.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class informations : MonoBehaviour {
+    [SerializeField] public GameObject canvas;
+	// Use this for initialization
+	void Start () {
+        canvas.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "info_plat")
+        {
+            canvas.SetActive(true);
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "info_plat")
+        {
+            canvas.SetActive(false);
+        }
+    }
+}
