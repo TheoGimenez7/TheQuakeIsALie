b1563565dcc4aa1cedcb18326925139053293a9c
LordChicken <theogimenez7@gmail.com>
2019-06-07 08:45:33 +0200

    TESTE Tab multi V1

--- Assets/Scripts/Menu/JoueurScoreBoardObjetScript.cs
+++ Assets/Scripts/Menu/JoueurScoreBoardObjetScript.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Networking;
 [RequireComponent(typeof(Player))]
-public class JoueurScoreBoardObjetScript : MonoBehaviour {
+public class JoueurScoreBoardObjetScript : NetworkBehaviour {
     [SerializeField]
     Text usernameText;
     [SerializeField]
