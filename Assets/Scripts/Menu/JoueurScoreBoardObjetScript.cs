f8beb2dfc9ed4ae72daf778b2f78296da5bb3020
LordChicken <theogimenez7@gmail.com>
2019-06-07 08:42:05 +0200

    Update JoueurScoreBoardObjetScript.cs

--- Assets/Scripts/Menu/JoueurScoreBoardObjetScript.cs
+++ Assets/Scripts/Menu/JoueurScoreBoardObjetScript.cs
@@ -1,10 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Networking;
 using UnityEngine.UI;
 [RequireComponent(typeof(Player))]
-public class JoueurScoreBoardObjetScript : NetworkBehaviour {
+public class JoueurScoreBoardObjetScript : MonoBehaviour {
     [SerializeField]
     Text usernameText;
     [SerializeField]
@@ -12,7 +11,6 @@ public class JoueurScoreBoardObjetScript : NetworkBehaviour {
     [SerializeField]
     Text deathsText;
 
-    [Command]
     public void Setup(string username , int kills , int deaths)
     {
         usernameText.text = username;
